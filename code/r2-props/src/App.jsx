e44fc61bb91b3f3c47aa02e0081733ee824b4edc
Helder Pereira <phelder@gmail.com>
2023-11-06 19:49:53 +0000

    props

---
+++ code/r2-props/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card"
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Card name="Helder" age="42" city="Porto" profession="Teacher" />
+      <Card name="Ricardo" />
+      <Card name="Diogo" />
+    </>
+  )
+}
+
+export default App
---
+++ code/r2-props/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card(props) {  
+
+    return(
+        <>
+            <p className="card">{ props.name }</p>
+        </>
+    )
+}
+
+export default Card;
