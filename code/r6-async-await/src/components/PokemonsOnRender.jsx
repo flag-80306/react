5a94ba108348125e03e11e0c035fdd785cfb5216
Helder Pereira <phelder@gmail.com>
2023-11-13 17:18:25 +0000

    last changes

--- code/r6-async-await/src/components/PokemonsOnRender.jsx
+++ code/r6-async-await/src/components/PokemonsOnRender.jsx
@@ -4,7 +4,7 @@ function PokemonsOnRender() {
   const [pokeList, setPokeList] = useState([]);
 
   async function getPokemon() {
-    const url = "https://pokeapi.co/api/v2/pokemon";
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=2000";
 
     const response = await fetch(url);
     const result = await response.json();
@@ -19,8 +19,8 @@ function PokemonsOnRender() {
   return (
     <>
       <ul>
-        {pokeList.map(function(result) {
-          return <li>{result.name}</li>
+        {pokeList.map(function(result, index) {
+          return <li>{index} - {result.name}</li>
         })}
       </ul>
     </>
